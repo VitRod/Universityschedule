adda4ebb2f28220c394692c7a1c4dc42ec66e07e
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-30 16:59:27 +0300

    Hello world  Greeting using thymeleaf

--- src/main/java/com/vit/controller/HelloWorldController.java
+++ src/main/java/com/vit/controller/HelloWorldController.java
@@ -1,12 +1,12 @@
 package com.vit.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HelloWorldController {
-	@RequestMapping("/")
-	public String hello() {
-		return "Hello Vit in java ";
-	}
-}
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class HelloWorldController {
+//	@RequestMapping("/")
+//	public String hello() {
+//		return "Hello Vit in java ";
+//	}
+//}
