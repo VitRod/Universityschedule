f23abde9b180bc20efde47b61b0325726189db47
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-30 14:04:33 +0300

    first commit

---
+++ src/main/java/com/vit/UniversityscheduleApplication.java
@@ -0,0 +1,13 @@
+package com.vit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityscheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityscheduleApplication.class, args);
+	}
+
+}
